e47b8ae2fbdfd191fc5afa04236b4e5b8e6ced53
NonExistPlayer <nonexistplayer@gmail.com>
2026-03-24 13:55:32 +0300

    chore: license

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
-}
+/*
+ *  spyland — screen time for Wayland
+ *  Copyright (C) 2026 Ilya Korobov (NonExistPlayer)
+ *  Licensed under the GNU General Public License v3.0
+ */
+
+fn main() {}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/*
+ *  spyland — screen time for Wayland
+ *  Copyright (C) 2026 Ilya Korobov (NonExistPlayer)
+ *  Licensed under the GNU General Public License v3.0
+ */
