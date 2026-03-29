c6e71fd152a71ee408c841157f9089d043945327
NonExistPlayer <nonexistplayer@gmail.com>
2026-03-29 15:04:05 +0300

    feat(core): implement basic backend abstraction

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,3 +3,32 @@
  *  Copyright (C) 2026 Ilya Korobov (NonExistPlayer)
  *  Licensed under the GNU General Public License v3.0
  */
+
+use std::sync::mpsc::Receiver;
+
+pub struct Session {
+    utc_start: i64,
+    utc_end: i64,
+
+    state: State,
+}
+
+pub enum State {
+    Active {
+        app_id: String,
+        workspace: i32,
+        // activity: ???,
+    },
+    Idle,
+}
+
+pub enum Event {
+    ActiveWindowChanged(Option<String>),
+    WorkspaceChanged(i32),
+    Idle(bool),
+}
+
+pub trait Backend {
+    fn is_available() -> bool;
+    fn subscribe(&mut self) -> Receiver<Event>;
+}
