bfcd5aaf9359ebfd435f5ee43855414a10601d76
NonExistPlayer <nonexistplayer@gmail.com>
2026-03-31 11:17:31 +0300

    feat(core): add `SessionManager`

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -27,6 +27,65 @@ impl Session {
         self.utc_start <= 0 && self.utc_end <= 0
     }
 }
+
+pub struct SessionManager<C: Clock> {
+    current: Session,
+    workspace: i32,
+    clock: C,
+    sessions: Vec<Session>,
+}
+
+impl<C: Clock> SessionManager<C> {
+    pub fn new(clock: C) -> Self {
+        Self {
+            clock,
+            workspace: -1,
+            current: Session::new_empty(),
+            sessions: Vec::new(),
+        }
+    }
+
+    pub fn handle_event(&mut self, event: Event) {
+        match event {
+            Event::ActiveWindowChanged(a) => {
+                let Some(app_id) = a else {
+                    return;
+                };
+
+                self.new_session();
+
+                let now = self.clock.now();
+                self.current.utc_start = now;
+                self.current.state = State::Active {
+                    app_id: app_id,
+                    workspace: self.workspace,
+                };
+            }
+            Event::WorkspaceChanged(id) => {
+                self.workspace = id;
+            }
+            Event::Idle(_) => {
+                // TODO:
+            }
+            Event::Tick => {
+                self.current.utc_end = self.clock.now();
+            }
+        }
+    }
+
+    fn new_session(&mut self) {
+        if !self.current.is_empty() {
+            self.sessions.push(self.current.clone());
+            self.current = Session::new_empty();
+        }
+    }
+}
+
+pub trait Clock {
+    fn now(&self) -> i64;
+}
+
+#[derive(Clone)]
 pub enum State {
     Active {
         app_id: String,
@@ -40,6 +99,7 @@ pub enum Event {
     ActiveWindowChanged(Option<String>),
     WorkspaceChanged(i32),
     Idle(bool),
+    Tick,
 }
 
 pub trait Backend {
